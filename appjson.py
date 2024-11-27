d12d3d6f2f904dedc068b23bf9ccd97206306131
xangelmex11 <angelloxaar@gmail.com>
2024-11-27 00:22:31 -0600

    Update appjson.py

--- appjson.py
+++ appjson.py
@@ -146,5 +146,5 @@ def index():
     return jsonify({"message": "Bienvenido a la API del Mercado2"}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 4000))
+    port = int(os.environ.get("PORT", 3000))  # Cambié el puerto predeterminado a 3000
     app.run(host="0.0.0.0", port=port, debug=True)
